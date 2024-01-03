4e94f5aeee0ebdbc847d93e4eed4dbc01f4a27a0
Vadim <VadimPokhabov@mail.ru>
2024-01-03 15:55:05 +0300

    add func2 add docstring

--- str_func.py
+++ str_func.py
@@ -1,5 +1,12 @@
 def func():
+    """Все буквы заглавные"""
     text = input('Введите слово')
     return text.upper()
 
+
 print(func())
+
+def func2(text):
+    """Принимает текст,
+    возвращает с большой буквы"""
+    return text.title()
